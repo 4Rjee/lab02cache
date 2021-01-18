3cb1b0735c128fab9540646eb83a2c98b0f9dcbd
4Rjee <slavasleychezB4trider>
2021-01-19 02:47:46 +0300

    added code

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,15 @@
 
 #include <gtest/gtest.h>
 
+#include <cache_exploration.hpp>
+
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+  cache_exploration cache;
+  cache.direct_travel();
+  std::cout << cache;
+  cache.reverse_travel();
+  std::cout << cache;
+  cache.random_travel();
+  std::cout << cache;
+  EXPECT_TRUE(true);
 }
