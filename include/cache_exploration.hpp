d851b9aeb6ec73c39dfb96cda41078603ca8223b
4Rjee <slavasleychezB4trider>
2021-01-19 10:10:50 +0300

    added copyright

--- include/cache_exploration.hpp
+++ include/cache_exploration.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Kochurin Nikita kochurin.nikita@gmail.com
 
 #ifndef INCLUDE_CACHE_EXPLORATION_HPP_
 #define INCLUDE_CACHE_EXPLORATION_HPP_
--- sources/cache_exploration.cpp
+++ sources/cache_exploration.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Kochurin Nikita kochurin.nikita@gmail.com
 
 #include "cache_exploration.hpp"
 
